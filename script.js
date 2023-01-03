b59daaefb0995b3abb2bad88112c3be968fe16b6
Michael Reyna <25.27.reyna@gmail.com>
2023-01-03 15:23:10 -0700

    added worddisplay doctype

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var startBtn = document.querySelector("#start-game");
 var timer = document.querySelector("#timer");
+var wordDisplay = document.querySelector("#word-display");
 var time = 60;
 
 startBtn.addEventListener("click", function(){
     // run game function here
 })
 
+
+
+//Timer
 var countDown = setInterval(() => {
     time--;
     timer.textContent = time + "s";
